901b0db2862a10b029c5b93a230cf9de4f6f5191
Leah <LeahKahwadjian@gmail.com>
2020-04-27 00:42:15 -0400

    built component structure and some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Grid from './components/Grid';
-import './App.css';
+import React from "react";
+import Grid from "./components/Grid";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>leah</h2>
       <Grid />
     </div>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import activities from '../data';
-import Card from './Card'
+import React from "react";
+import activities from "../data";
+import Card from "./Card";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFireAlt,
+  faUtensils,
+  faCocktail,
+  faBinoculars,
+  faShoppingCart,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Grid = () => {
-    return (
+  return (
+    <div className="app-container">
+      <section>
+        <div className="container-filters">
+          <button className="button button--filter">
+            <FontAwesomeIcon icon={faFireAlt} size="2x" />
+          </button>
+          <button className="button button--filter">
+            <FontAwesomeIcon icon={faUtensils} size="2x" />
+          </button>
 
-        <div>
-            {activities.map(activity => (
-                <Card data={activity} />
-
-            ))}
+          <button className="button button--filter">
+            <FontAwesomeIcon icon={faCocktail} size="2x" />
+          </button>
+          <button className="button button--filter">
+            <FontAwesomeIcon icon={faBinoculars} size="2x" />
+          </button>
+          <button className="button button--filter">
+            <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+          </button>
         </div>
-    );
+      </section>
+      <section>
+        <div className="grid">
+          {activities.map((activity) => (
+            <Card data={activity} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Grid;
