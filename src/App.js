efd354e8448c5f6f1bb6b4b2585225e5e983e1da
Leah <LeahKahwadjian@gmail.com>
2020-05-12 23:24:42 -0400

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "./components/Grid";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb as farLightbulb } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setDarkMode((prevMode) => !darkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <FontAwesomeIcon
+        onClick={handleDarkMode}
+        icon={darkMode ? faLightbulb : farLightbulb}
+        size="2x"
+      />
+
+      <h1>{darkMode ? "dark" : "light"}</h1>
       <Grid />
     </div>
   );
